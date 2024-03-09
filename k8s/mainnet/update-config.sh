443de0e02444a26d74756ce119acd8796c3843a5
Scholtz <ludko2@gmail.com>
2024-03-09 21:46:17 +0100

    cicd deploy

--- k8s/mainnet/update-config.sh
+++ k8s/mainnet/update-config.sh
@@ -2,3 +2,4 @@ kubectl apply -f deployment-main.yaml -n biatec
 kubectl delete configmap biatec-nfts-main-conf -n biatec
 kubectl create configmap biatec-nfts-main-conf --from-file=conf -n biatec
 kubectl rollout restart deployment/biatec-nfts-main-deployment -n biatec
+kubectl rollout status deployment/biatec-nfts-main-deployment -n biatec -w 
